9785434fedec07d0b2a74893bb0eb352270b39c6
Dobrovenskis <dobrovenskis.rv@phystech.edu>
2020-11-27 12:37:38 +0300

    Изменил миникарту.

--- minimap.py
+++ minimap.py
@@ -5,13 +5,26 @@ from ray_casting import *
 
 
 def draw_minimap(player, screen):
+    '''
+    Рисует миникарту.
+    :param player: игрок
+    :param screen: экран, на котором будет карта
+    '''
+    # Рисование бэкграунда
+    pygame.draw.rect(screen, Black, ((0, 0), (len(text_map[1])*cube // scale_minimap, len(text_map)*cube // scale_minimap)))
+    #Рисование игрока
     x_pl = int(player.x_player) // scale_minimap
     y_pl = int(player.y_player) // scale_minimap
-    pygame.draw.rect(screen, Black, ((0, 0), (width_screen // scale_minimap, height_screen // scale_minimap)))
     pygame.draw.circle(screen, Green, (x_pl, y_pl), player_radius)
     pygame.draw.line(screen, Yellow, (x_pl, y_pl),
                      (x_pl + player_length * np.cos(player.angle),
                       y_pl + player_length * np.sin(player.angle)), player_width)
+
+    #Рисование кубиков
     for x, y in map:
-        pygame.draw.rect(screen, Red,
-                         (x // scale_minimap, y // scale_minimap, cube // scale_minimap, cube // scale_minimap))
+        if map[(x, y)] == '1':
+            pygame.draw.rect(screen, Red,
+                             (x // scale_minimap, y // scale_minimap, cube // scale_minimap, cube // scale_minimap))
+        if map[(x, y)] == '2':
+            pygame.draw.rect(screen, Green,
+                             (x // scale_minimap, y // scale_minimap, cube // scale_minimap, cube // scale_minimap))
